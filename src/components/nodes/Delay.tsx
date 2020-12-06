924bfd3901fa7d2b2b80d3f09b902ec25c1d2118
Michał Ochman <michoch@gmail.com>
2020-12-06 15:19:24 +0100

    Add option to change Delay max delay time

--- src/components/nodes/Delay.tsx
+++ src/components/nodes/Delay.tsx
@@ -4,12 +4,16 @@ import { AudioContext } from "context/AudioContext";
 import { useNode } from "context/NodeContext";
 import Node from "components/Node";
 
+function clampDelayTime(delayTime: number) {
+  return Math.max(0.001, Math.min(179.999, delayTime));
+}
+
 function Delay({ data, id, selected, type }: NodeProps) {
-  const { delayTime = 1, onChange } = data;
+  const { delayTime = 1, maxDelayTime = 1, onChange } = data;
 
   // AudioNode
   const context = useContext(AudioContext);
-  const node = useMemo<DelayNode>(() => context.createDelay(), [context]);
+  const node = useMemo<DelayNode>(() => context.createDelay(clampDelayTime(maxDelayTime)), [context, maxDelayTime]);
   useNode(id, node);
 
   // AudioParam
@@ -22,9 +26,22 @@ function Delay({ data, id, selected, type }: NodeProps) {
           <div className="customNode_item">
             <input
               className="nodrag"
-              type="number"
+              max={node.delayTime.maxValue}
+              min={node.delayTime.minValue}
+              step={0.1}
               onChange={e => onChange({ delayTime: +e.target.value })}
+              type="range"
               value={delayTime}
+            />
+          </div>
+          <div className="customNode_item">
+            <input
+              className="nodrag"
+              onChange={e => onChange({ maxDelayTime: +e.target.value })}
+              max={0}
+              min={180}
+              type="number"
+              value={clampDelayTime(maxDelayTime)}
             />
           </div>
         </div>
