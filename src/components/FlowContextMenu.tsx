2f1f39a3a0ec2e4a6069e3bddfca12240e7375fe
Michał Ochman <michoch@gmail.com>
2021-01-11 21:54:42 +0100

    Change context menu to be multi level

--- src/components/FlowContextMenu.tsx
+++ src/components/FlowContextMenu.tsx
@@ -7,28 +7,53 @@ interface Props {
 function FlowContextMenu({ addNode }: Props) {
   return (
     <ul className="contextMenu">
-      <li onClick={() => addNode("ADSR")}>Add ADSR</li>
-      <li onClick={() => addNode("Analyser")}>Add Analyser</li>
-      <li onClick={() => addNode("BiquadFilter")}>Add Biquad Filter</li>
-      <li onClick={() => addNode("ChannelMerger")}>Add Channel Merger</li>
-      <li onClick={() => addNode("ChannelSplitter")}>Add Channel Splitter</li>
-      <li onClick={() => addNode("ConstantSource")}>Add Constant Source</li>
-      <li onClick={() => addNode("Delay")}>Add Delay</li>
-      <li onClick={() => addNode("Destination")}>Add Destination</li>
-      <li onClick={() => addNode("DynamicsCompressor")}>Add Dynamics Compressor</li>
-      <li onClick={() => addNode("Envelope")}>Add Envelope</li>
-      <li onClick={() => addNode("DelayEffect")}>Add DelayEffect</li>
-      <li onClick={() => addNode("Gain")}>Add Gain</li>
-      <li onClick={() => addNode("Gate")}>Add Gate</li>
-      <li onClick={() => addNode("InputSwitch")}>Add Input Switch</li>
-      <li onClick={() => addNode("Keyboard")}>Add Keyboard</li>
-      <li onClick={() => addNode("Metronome")}>Add Metronome</li>
-      <li onClick={() => addNode("Noise")}>Add Noise</li>
-      <li onClick={() => addNode("Oscillator")}>Add Oscillator</li>
-      <li onClick={() => addNode("OscillatorNote")}>Add Oscillator Note</li>
-      <li onClick={() => addNode("OutputSwitch")}>Add Output Switch</li>
-      <li onClick={() => addNode("StereoPanner")}>Add Stereo Panner</li>
-      <li onClick={() => addNode("WaveShaper")}>Add Wave Shaper</li>
+      <li>
+        Sources
+        <ul className="contextMenu sub">
+          <li onClick={() => addNode("Gate")}>Gate</li>
+          <li onClick={() => addNode("Keyboard")}>Keyboard</li>
+          <li onClick={() => addNode("Metronome")}>Metronome</li>
+          <li onClick={() => addNode("Noise")}>Noise</li>
+          <li onClick={() => addNode("Oscillator")}>Oscillator</li>
+          <li onClick={() => addNode("OscillatorNote")}>Oscillator Note</li>
+        </ul>
+      </li>
+      <li>
+        Destinations
+        <ul className="contextMenu sub">
+          <li onClick={() => addNode("Destination")}>Destination</li>
+        </ul>
+      </li>
+      <li>
+        Effects
+        <ul className="contextMenu sub">
+          <li onClick={() => addNode("BiquadFilter")}>Biquad Filter</li>
+          <li onClick={() => addNode("Delay")}>Delay</li>
+          <li onClick={() => addNode("DelayEffect")}>Delay Effect</li>
+          <li onClick={() => addNode("DynamicsCompressor")}>Dynamics Compressor</li>
+          <li onClick={() => addNode("Gain")}>Gain</li>
+          <li onClick={() => addNode("StereoPanner")}>Stereo Panner</li>
+          <li onClick={() => addNode("WaveShaper")}>Wave Shaper</li>
+          <li onClick={() => addNode("ChannelMerger")}>Channel Merger</li>
+          <li onClick={() => addNode("ChannelSplitter")}>Channel Splitter</li>
+        </ul>
+      </li>
+      <li>
+        Controllers
+        <ul className="contextMenu sub">
+          <li onClick={() => addNode("ADSR")}>ADSR</li>
+          <li onClick={() => addNode("ConstantSource")}>Constant Source</li>
+          <li onClick={() => addNode("Envelope")}>Envelope</li>
+          <li onClick={() => addNode("InputSwitch")}>Input Switch</li>
+          <li onClick={() => addNode("OutputSwitch")}>Output Switch</li>
+        </ul>
+      </li>
+      <li>
+        Analysers
+        <ul className="contextMenu sub">
+          <li onClick={() => addNode("Analyser")}>Analyser</li>
+        </ul>
+      </li>
     </ul>
   );
 }
